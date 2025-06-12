131775dfd4f2d64972b594e21c8435932befeeef
lamtota40 <lamtota40@gmail.com>
2025-06-12 19:10:58 +0700

    Update setup-grml.sh

--- setup-grml.sh
+++ setup-grml.sh
@@ -15,7 +15,7 @@ sudo tee /grml.sh > /dev/null << 'EOF'
 EOF
 sudo chmod +x /grml.sh
 
-sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh myconfig=/dev/vda3 scripts=/grml.sh toram\"' >> /etc/default/grml-rescueboot"
+sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh toram\"' >> /etc/default/grml-rescueboot"
 sudo update-grub
 sudo grub-reboot 'Grml Rescue System (grml-small-2024.12-amd64.iso)'
 read -p "tekan [ENTER] untuk reboot"
