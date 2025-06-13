c80f4d150322a3cba4a55e75da8aed5d3c5361d3
lamtota40 <lamtota40@gmail.com>
2025-06-13 13:03:18 +0700

    Update setup-grml.sh

--- setup-grml.sh
+++ setup-grml.sh
@@ -8,15 +8,15 @@ sudo mkdir -p /boot/grml
   sudo wget https://mirror-hk.koddos.net/grml/grml-small-2024.12-amd64.iso -P /boot/grml/ --no-check-certificate
  fi
  
-sudo tee /grml.sh > /dev/null << 'EOF'
+#sudo tee /grml.sh > /dev/null << 'EOF'
 #!/bin/bash
 #put yourscript here for executioon boot GRML and eanable myconfig=/dev/vda3 scripts=/grml.sh
 #dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/user../autorun-grml.sh
-touch /sukses.txt
-EOF
-sudo chmod +x /grml.sh
+#touch /sukses.txt
+#EOF
+#sudo chmod +x /grml.sh
 
-sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh myconfig=/dev/vda1 scripts=/grml.sh toram\"' >> /etc/default/grml-rescueboot"
+sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh toram\"' >> /etc/default/grml-rescueboot"
 sudo update-grub
 sudo grub-reboot 'Grml Rescue System (grml-small-2024.12-amd64.iso)'
 read -p "tekan [ENTER] untuk reboot"
